2f422720d461939515dcaf616b108f2265148063
張庭瑜 <u112029011@gap.kmu.edu.tw>
2025-11-24 13:12:05 +0800

    暫存更改

--- new_heatmap_0.py
+++ new_heatmap_0.py
@@ -3,41 +3,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# 1. 載入原始資料
-df = pd.read_csv("diabetes_with_log_values.csv")  # 用完整資料才看得出差異！
+# 1. 載入完整資料（包含 outcome=0 和 1）
+df = pd.read_csv("diabetes_with_log_values.csv")
 
-# 2. 依照 Outcome 分組
+# 2. 分組
 df0 = df[df["Outcome"] == 0].drop(columns=["Outcome"])
 df1 = df[df["Outcome"] == 1].drop(columns=["Outcome"])
 
-# 3. 分別計算相關係數矩陣
+# 3. 計算相關係數矩陣
 corr0 = df0.corr(method='pearson')
 corr1 = df1.corr(method='pearson')
 
-
-# 4. 畫 Outcome=0 熱力圖
+# 4. Outcome=0 熱力圖
 plt.figure(figsize=(10, 8))
 sns.heatmap(
-    corr0,
-    annot=True,
-    fmt=".2f",
-    cmap="coolwarm",
-    linewidths=.5
+    corr0, annot=True, fmt=".2f",
+    cmap="coolwarm", linewidths=.5
 )
 plt.title("Correlation Heatmap (Outcome = 0)")
+plt.tight_layout()
 plt.savefig("heatmap_outcome_0.png")
-plt.show()
-
-
-# 5. 畫 Outcome=1 熱力圖
-plt.figure(figsize=(10, 8))
-sns.heatmap(
-    corr1,
-    annot=True,
-    fmt=".2f",
-    cmap="coolwarm",
-    linewidths=.5
-)
-plt.title("Correlation Heatmap (Outcome = 1)")
-plt.savefig("heatmap_outcome_1.png")
-plt.show()
+plt.clos
